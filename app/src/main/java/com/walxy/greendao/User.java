0adaeb6a54c07fef313b5c36d2a0c83c7c916714
SH <1450497718@qq.com>
2017-10-30 18:30:08 +0800

    GreenDao数据库

---
+++ app/src/main/java/com/walxy/greendao/User.java
@@ -0,0 +1,42 @@
+package com.walxy.greendao;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.Id;
+
+/**
+ * 先写实体类User
+ */
+@Entity
+public class User {
+
+    @Id
+    private long id;
+    private String name;
+
+    @Generated(hash = 586692638)
+    public User() {
+    }
+
+    @Generated(hash = 1144922831)
+    public User(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
